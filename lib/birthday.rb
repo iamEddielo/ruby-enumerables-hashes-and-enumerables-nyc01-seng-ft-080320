00729c24c36c12c9b6a681c7366c607360a1eb6d
Eddie Lopez <iameddielopez@gmail.com>
2020-07-21 19:19:24 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,7 +8,7 @@ def happy_birthday(birthday_kids)
   # add your code snippet here!
   
   birthday_kids.each do |name , birthday_age|
-    puts " Happy Birthday #{name}! You are now #{birthday_age} old!"
+    p " Happy Birthday #{name}! You are now #{birthday_age} old!"
   end
 end
 birthday_kids
